d7483c73eb9c8579046dda99f84e1a1d54ddafa5
Stephen Chong <schong@cs.cornell.edu>
2005-04-11 16:06:51 +0000

    Make use of label checking of access paths.

--- src/jif/extension/JifActsForExt.java
+++ src/jif/extension/JifActsForExt.java
@@ -2,7 +2,14 @@ package jif.extension;
 
 import jif.ast.ActsFor;
 import jif.translate.ToJavaExt;
-import jif.types.*;
+import jif.types.JifContext;
+import jif.types.JifTypeSystem;
+import jif.types.PathMap;
+import jif.types.label.*;
+import jif.types.label.AccessPath;
+import jif.types.label.AccessPathRoot;
+import jif.types.label.Label;
+import jif.types.principal.DynamicPrincipal;
 import jif.types.principal.Principal;
 import jif.visit.LabelChecker;
 import polyglot.ast.Node;
@@ -16,61 +23,62 @@ public class JifActsForExt extends JifStmtExt_c
     public JifActsForExt(ToJavaExt toJava) {
         super(toJava);
     }
-
+    
     /** Label check the <code>actsFor</code> statement.
      *  Refer to Figure 4.25
      */
     public Node labelCheckStmt(LabelChecker lc) throws SemanticException
     {
-	ActsFor af = (ActsFor) node();
-
+        ActsFor af = (ActsFor) node();
+        
         JifContext A = lc.jifContext();
         A = (JifContext) af.enterScope(A);
         JifTypeSystem ts = lc.jifTypeSystem();
-
+        
         Principal p1 = af.actor().principal();
         Principal p2 = af.granter().principal();
-
-        PathMap X1 = ts.pathMap().N(A.pc());
-        PathMap X2 = ts.pathMap().N(A.pc());
-
-        //@@@@@ just get it to compile first...
-//        if (p1 instanceof DynamicPrincipal) {
-//            // <pr-dynamic uid L> was <var final principal{L} uid>
-//            // Simulate the effect of the variable lookup.
-//            Label L = ((DynamicPrincipal) p1).label();
-//            X1 = X1.NV(L.join(A.pc()));
-//        }
-
+        
+        PathMap X1;
+        if (p1 instanceof DynamicPrincipal) {
+            // Get the information given by the successful evaluation of the access path            
+            AccessPath path = ((DynamicPrincipal) p1).path();
+            X1 = path.labelcheck(A);
+        }
+        else {
+            X1 = ts.pathMap().N(A.pc());            
+        }
+        
         A = (JifContext) A.pushBlock();
         A.setPc(X1.N());
-
-        //@@@@@ just get it to compile first...
-//        if (p2 instanceof DynamicPrincipal) {
-//            // <pr-dynamic uid L> was <var final principal{L} uid>
-//            // Simulate the effect of the variable lookup.
-//            Label L = ((DynamicPrincipal) p2).label();
-//            X2 = X2.NV(L.join(A.pc()));
-//        }
-
+        
+        PathMap X2;        
+        if (p2 instanceof DynamicPrincipal) {
+            // Get the information given by the successful evaluation of the access path            
+            AccessPath path = ((DynamicPrincipal) p2).path();
+            X2 = path.labelcheck(A);
+        }
+        else {
+            X2 = ts.pathMap().N(A.pc());            
+        }
+        
         A = (JifContext) A.pop();
-
+        
         A = (JifContext) A.pushBlock();
-
+        
         A.setPc(X1.NV().join(X2.NV()));
-
+        
         A = (JifContext) A.pushBlock();
         A.addActsFor(p1, p2);
-
+        
         Stmt S1 = (Stmt) lc.context(A).labelCheck(af.consequent());
         PathMap X3 = X(S1);
-
+        
         A = (JifContext) A.pop();
-
+        
         PathMap X4; 
-
+        
         Stmt S2 = null;
-
+        
         if (af.alternative() != null) {
             S2 = (Stmt) lc.context(A).labelCheck(af.alternative());
             X4 = X(S2);
@@ -78,11 +86,11 @@ public class JifActsForExt extends JifStmtExt_c
         else {
             X4 = ts.pathMap().N(A.pc());
         }
-
+        
         A = (JifContext) A.pop();
-
+        
         PathMap X = X1.join(X2).join(X3).join(X4);
-
+        
         return X(af.consequent(S1).alternative(S2), X);
     }
 }
