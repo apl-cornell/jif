78bc930732de6765928f944ea16e8dbda4254a79
Stephen Chong <schong@cs.cornell.edu>
2005-04-13 21:06:08 +0000

    Fixed bug with determining if a field of an instantiated class has been constant checked.

--- src/jif/JifScheduler.java
+++ src/jif/JifScheduler.java
@@ -4,11 +4,16 @@ import java.util.Iterator;
 
 import jif.ast.JifNodeFactory;
 import jif.translate.JifToJavaRewriter;
+import jif.types.JifSubstClassType_c;
+import jif.types.JifSubstType;
 import jif.types.JifTypeSystem;
 import polyglot.ext.jl.JLScheduler;
 import polyglot.frontend.Job;
+import polyglot.frontend.goals.FieldConstantsChecked;
 import polyglot.frontend.goals.Goal;
 import polyglot.frontend.goals.VisitorGoal;
+import polyglot.types.FieldInstance;
+import polyglot.types.ParsedClassType;
 
 public class JifScheduler extends JLScheduler {
    OutputExtensionInfo jlext;
@@ -35,6 +40,9 @@ public class JifScheduler extends JLScheduler {
         JifNodeFactory nf = (JifNodeFactory) extInfo.nodeFactory();
         return internGoal(new VisitorGoal(job, new JifToJavaRewriter(job, ts, nf, jlext)));        
     }
+    public Goal FieldConstantsChecked(FieldInstance fi) {
+        return internGoal(new JifFieldConstantsChecked(fi));
+    }
     public boolean runToCompletion() {
         if (super.runToCompletion()) {
             // Create a goal to compile every source file.
@@ -45,5 +53,20 @@ public class JifScheduler extends JLScheduler {
             return jlext.scheduler().runToCompletion();
         }
         return false;
+    }
+    
+    private static class JifFieldConstantsChecked extends FieldConstantsChecked {
+        public JifFieldConstantsChecked(FieldInstance fi) {
+            super(fi);
+        }
+        protected ParsedClassType findContainer() {
+            if (var().container() instanceof JifSubstType) {
+                JifSubstType jst = (JifSubstType)var().container();
+                if (jst.base() instanceof ParsedClassType) {
+                    return (ParsedClassType) jst.base();
+                }
+            }
+            return super.findContainer();
+        }        
     }
 }
