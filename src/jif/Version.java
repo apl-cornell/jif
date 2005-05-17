ceba2a1740183db117c8e204308dd24fead10941
Stephen Chong <schong@cs.cornell.edu>
2005-05-17 18:19:06 +0000

    Some work on user documentation. I should get around to updating the html files in the doc directory...

--- src/jif/Version.java
+++ src/jif/Version.java
@@ -2,7 +2,7 @@ package jif;
 
 public class Version extends polyglot.main.Version {
     public String name() { return "jif"; }
-    public int major() 	{ return 1; }
-    public int minor() 	{ return 1; }
-    public int patch_level() 	{ return 1; }
+    public int major() 	{ return 2; }
+    public int minor() 	{ return 0; }
+    public int patch_level() 	{ return 0; }
 }
