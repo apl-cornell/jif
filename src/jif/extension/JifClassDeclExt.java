2b743504db0d446e16a1ddce4e35b2c2a9a7b1e2
Stephen Chong <schong@cs.cornell.edu>
2005-10-11 17:37:20 +0000

    Bug in Jif where certain label override conformance checks were checking the pair of methods the wrong way round.

--- src/jif/extension/JifClassDeclExt.java
+++ src/jif/extension/JifClassDeclExt.java
@@ -116,7 +116,7 @@ public class JifClassDeclExt extends Jif_c {
                         // includes access protection checks.
                         if (mj.canOverrideImpl(mi, true)) {
                             // passes the java checks, now perform the label checks                                        
-                            CallHelper.OverrideHelper(mj, mi, lc).checkOverride(lc);
+                            CallHelper.OverrideHelper(mi, mj, lc).checkOverride(lc);
                         }
                     }
                     catch (SemanticException e) {
