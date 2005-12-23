8b8da4356b49824b8fbe8ef9ce58ba3b70da584f
Stephen Chong <schong@cs.cornell.edu>
2005-12-23 00:16:03 +0000

    Make use of the more expressive principals.

--- rt-src/jif/lang/ReadableByPrinPolicy.java
+++ rt-src/jif/lang/ReadableByPrinPolicy.java
@@ -2,45 +2,16 @@ package jif.lang;
 
 import java.util.*;
 
-public class ReadableByPrinPolicy implements ConfPolicy
+public class ReadableByPrinPolicy extends ReaderPolicy
 {
-    private final Principal reader;
-
-    public ReadableByPrinPolicy(Principal reader) {
-	this.reader = reader;
-    }
-
-    public Principal reader() {
-	return reader;
-    }
     
-    public boolean relabelsTo(Policy l) {
-        if (l instanceof ReadableByPrinPolicy) {
-            ReadableByPrinPolicy rbp = (ReadableByPrinPolicy)l;
-            return PrincipalUtil.actsFor(rbp.reader, this.reader);
-        }
-        return false;
+    public ReadableByPrinPolicy(Principal reader) {
+        super(PrincipalUtil.topPrincipal(), reader);
     }
 
-
-    public int hashCode() {
-	return (reader==null?0:reader.hashCode());
-    }
-
-    public boolean equals(Object o) {
-	if (! (o instanceof ReadableByPrinPolicy)) {
-	    return false;
-	}
-
-	ReadableByPrinPolicy that = (ReadableByPrinPolicy) o;
-
-	return (this.reader == that.reader || 
-	        (reader != null && reader.equals(that.reader)
-            && that.reader != null && that.reader.equals(reader))); 
-    }
     
     public String componentString() {
-        return "[readable by " + (reader == null?"<null>":reader.name()) + "]";
+        return "[readable by " + (PrincipalUtil.toString(reader())) + "]";
     }
 
 }
