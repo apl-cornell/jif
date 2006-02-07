c8e12e4398400762e81d69b12276ec233f740aee
Stephen Chong <schong@cs.cornell.edu>
2006-02-07 22:26:26 +0000

    Was incorrectly specialized for DeclassifyExprs

--- src/jif/translate/DowngradeExprToJavaExt_c.java
+++ src/jif/translate/DowngradeExprToJavaExt_c.java
@@ -10,12 +10,12 @@ import polyglot.visit.*;
 
 public class DowngradeExprToJavaExt_c extends ToJavaExt_c {
     public NodeVisitor toJavaEnter(JifToJavaRewriter rw) throws SemanticException {
-        DeclassifyExpr n = (DeclassifyExpr) node();
+        DowngradeExpr n = (DowngradeExpr) node();
         return rw.bypass(n.bound()).bypass(n.label());
     }
 
     public Node toJava(JifToJavaRewriter rw) throws SemanticException {
-        DeclassifyExpr n = (DeclassifyExpr) node();
+        DowngradeExpr n = (DowngradeExpr) node();
         return n.expr();
     }
 }
