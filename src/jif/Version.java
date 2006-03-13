7a2b52128ec7117c746a3b1f80a7fbe32d5a44da
Jed Liu <liujed@cs.cornell.edu>
2006-03-13 21:43:28 +0000

    This commit was manufactured by cvs2git to create tag 'JIF_RELEASE_2_0_1'.
    Sprout from master 2005-11-30 18:03:14 UTC Stephen Chong <schong@cs.cornell.edu> 'Removal of some dead code.'
    Cherrypick from master 2006-03-13 21:43:27 UTC Stephen Chong <schong@cs.cornell.edu> 'Updates for 2.0.1 release.':
        CHANGES
        examples/battleship/Coordinate.jif
        examples/battleship/Ship.jif
        src/jif/Version.java
    Delete:
        rt-src/jif/lang/AbstractLabel.java
        rt-src/jif/lang/JoinLabel.java
        rt-src/jif/lang/PrivacyPolicy.java
        rt-src/jif/lang/ReadableByPrinLabel.java
        src/jif/ast/PolicyLabelNode.java
        src/jif/ast/PolicyLabelNode_c.java
        src/jif/translate/DeclassifyExprToJavaExt_c.java
        src/jif/translate/DeclassifyStmtToJavaExt_c.java
        src/jif/translate/PolicyLabelToJavaExpr_c.java
        src/jif/types/label/PolicyLabel.java
        src/jif/types/label/PolicyLabel_c.java
        src/jif/types/label/TopLabel.java
        src/jif/types/label/TopLabel_c.java

--- src/jif/Version.java
+++ src/jif/Version.java
@@ -4,5 +4,5 @@ public class Version extends polyglot.main.Version {
     public String name() { return "jif"; }
     public int major() 	{ return 2; }
     public int minor() 	{ return 0; }
-    public int patch_level() 	{ return 0; }
+    public int patch_level() 	{ return 1; }
 }
--- src/jif/types/label/TopLabel.java
+++
@@ -1,9 +0,0 @@
-package jif.types.label;
-
-
-/**
- * The class <code>JifTopLabel</code> is the label at the top of the label
- * lattice.
- */
-public interface TopLabel extends Label {
-}
