49eeab5c1879dcc036c1079f87f7daa63bd23725
Stephen Chong <schong@cs.cornell.edu>
2007-07-17 20:35:27 +0000

    Bumped version to 3.1.0

--- src/jif/Version.java
+++ src/jif/Version.java
@@ -3,6 +3,6 @@ package jif;
 public class Version extends polyglot.main.Version {
     public String name() { return "jif"; }
     public int major() 	{ return 3; }
-    public int minor() 	{ return 0; }
+    public int minor() 	{ return 1; }
     public int patch_level() 	{ return 0; }
 }
