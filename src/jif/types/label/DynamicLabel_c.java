7680bbe295af6f616a5ef55407962ab2e0a3d84c
Krishnaprasad Vikram <kvikram@cs.cornell.edu>
2011-09-02 21:57:53 +0000

    Comment Typo.

--- src/jif/types/label/DynamicLabel_c.java
+++ src/jif/types/label/DynamicLabel_c.java
@@ -66,7 +66,7 @@ public class DynamicLabel_c extends Label_c implements DynamicLabel {
     }
 
     public boolean leq_(Label L, LabelEnv env, LabelEnv.SearchState state) {
-        // can be leq than L is L is also a dynamic label with an access path
+        // can be leq than L if L is also a dynamic label with an access path
         // equivalent to this one.
         if (L instanceof DynamicLabel) {
             DynamicLabel that = (DynamicLabel)L;
