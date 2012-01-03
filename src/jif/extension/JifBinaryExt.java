6d844e454bb1944aa5179c34c948acfe486ca42a
Michael George <mdgeorge@cs.cornell.edu>
2012-01-03 22:14:28 +0000

    started implementation of extended label checking for dynamic label and actsfor checks

--- src/jif/extension/JifBinaryExt.java
+++ src/jif/extension/JifBinaryExt.java
@@ -1,14 +1,17 @@
 package jif.extension;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
+import jif.ast.JifUtil;
 import jif.ast.Jif_c;
 import jif.translate.ToJavaExt;
 import jif.types.*;
 import jif.visit.LabelChecker;
 import polyglot.ast.*;
 import polyglot.types.SemanticException;
+import polyglot.qq.QQ;
 
 /** The Jif extension of the <code>Binary</code> node. 
  *  
@@ -22,21 +25,19 @@ public class JifBinaryExt extends JifExprExt
 
     public Node labelCheck(LabelChecker lc) throws SemanticException
     {
-        Binary be = (Binary) node();
-
         JifTypeSystem ts = lc.jifTypeSystem();
         JifContext A = lc.jifContext();
 
-        List throwTypes = new ArrayList(be.del().throwTypes(ts));
+        List throwTypes = new ArrayList(node().del().throwTypes(ts));
 
-        A = (JifContext) be.del().enterScope(A);
+        A = (JifContext) node().del().enterScope(A);
 
-        Expr left = (Expr) lc.context(A).labelCheck(be.left());
+        Expr left = (Expr) lc.context(A).labelCheck(node().left());
         PathMap Xl = getPathMap(left);
 
         A = (JifContext) A.pushBlock();
 
-        if (be.operator() == Binary.COND_AND || be.operator() == Binary.COND_OR) {
+        if (node().operator() == Binary.COND_AND || node().operator() == Binary.COND_OR) {
             // if it's a short circuit evaluation, then
             // whether the right is executed or not depends on the _value_
             // of the left sub-expression.
@@ -49,19 +50,46 @@ public class JifBinaryExt extends JifExprExt
             A.setPc(Xl.N(), lc);            
         }
 
-        Expr right = (Expr) lc.context(A).labelCheck(be.right());
+        Expr right = (Expr) lc.context(A).labelCheck(node().right());
         PathMap Xr = getPathMap(right);
 
         A = (JifContext) A.pop();
 
         PathMap X = Xl.N(ts.notTaken()).join(Xr);
 
-        if (be.throwsArithmeticException()) {
+        if (node().throwsArithmeticException()) {
             checkAndRemoveThrowType(throwTypes, ts.ArithmeticException());
             X = X.exc(Xr.NV(), ts.ArithmeticException());
         }
 
         checkThrowTypes(throwTypes);
-        return updatePathMap(be.left(left).right(right), X);
+        return updatePathMap(node().left(left).right(right), X);
+    }
+    
+    /**
+     * Check this node against constraints captured in a method signature.  More
+     * specifically, check that f(this.left,this.right) is a valid call.
+     */
+    protected Node labelCheckAsMethod(LabelChecker lc, JifProcedureInstance f) throws SemanticException
+    {
+        // TODO: because we are reusing the method call checking, the error
+        // messages produced will be confusing.  Fixing this will require some
+        // refactoring.
+        
+        QQ qq;
+        Expr e = qq.parseExpr("%M((%E),(%E))", f, node().left(), node().right());
+        Call result = (Call) lc.labelCheck(e);
+
+        Expr left  = (Expr) result.arguments().get(0);
+        Expr right = (Expr) result.arguments().get(1);
+        PathMap X  = JifUtil.jifExt(result).X();
+        
+        return updatePathMap(node().left(left).right(right), getPathMap(result));
+    }
+    
+    @Override
+    public Binary node()
+    {
+        return (Binary) super.node();
     }
 }
