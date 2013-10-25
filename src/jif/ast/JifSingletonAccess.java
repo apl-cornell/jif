7edd2bda39fb36e41d84134765c93b101b051a77
Matthew Loring <mcl83@cornell.edu>
2013-10-25 19:10:55 +0000

    Singleton ast nodes.

---
+++ src/jif/ast/JifSingletonAccess.java
@@ -0,0 +1,9 @@
+package jif.ast;
+
+import polyglot.ast.Expr;
+
+/** A reference to the singleton object housed by a Singleton class in Jif.
+ */
+public interface JifSingletonAccess extends Expr {
+   	
+}
