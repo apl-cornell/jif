10a69b7882b9b0f0a495a856cab603e5116ab27e
Matthew Loring <mcl83@cornell.edu>
2013-10-25 19:09:57 +0000

    First generics commit.

--- src/jif/types/ParamInstance.java
+++ src/jif/types/ParamInstance.java
@@ -61,7 +61,7 @@ public interface ParamInstance extends polyglot.ext.param.types.Param,
     public final static Kind PRINCIPAL = new Kind("principal", true, false, 
             false, false);
     public final static Kind TYPE = new Kind("type", false, false, false, 
-            false);
+            true);
 
     JifClassType container();
 
