519ed56a81d24fb7f9b0edcae49bee91786484e8
Jed Liu <liujed@cs.cornell.edu>
2013-10-31 19:04:25 -0400

    Fixed merge error.

--- src/jif/ast/JifSingletonAccess_c.java
+++ src/jif/ast/JifSingletonAccess_c.java
@@ -1,20 +1,20 @@
 package jif.ast;
 
-import polyglot.ast.Id;
+import java.util.List;
+
 import polyglot.ast.Expr;
 import polyglot.ast.TypeNode;
 import polyglot.util.Position;
 import polyglot.util.SerialVersionUID;
 
-import java.util.List;
-
 /** An implementation of the <code>JifSingletonAccess</code> interface.
  */
-public class JifSingletonAccess_c extends JifNew_c implements JifSingletonAccess {
+public class JifSingletonAccess_c extends JifNew_c implements
+        JifSingletonAccess {
     private static final long serialVersionUID = SerialVersionUID.generate();
 
     public JifSingletonAccess_c(Position pos, TypeNode objectType,
             List<Expr> args) {
-        super(pos, objectType, args, null);
+        super(pos, null, objectType, args, null);
     }
 }
