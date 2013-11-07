6883383a490beb2b3df2801ffa1fdccd1e4ae787
Matthew Loring <matthew.loring21@gmail.com>
2013-11-07 18:55:50 -0500

    nodes for classes as type parameter instantiations

--- src/jif/ast/TypeParamNode_c.java
+++ src/jif/ast/TypeParamNode_c.java
@@ -1,25 +1,34 @@
 package jif.ast;
 
+import java.util.List;
+
+import jif.types.JifTypeSystem;
 import jif.types.Param;
 import jif.types.TypeParam;
-import jif.types.TypeParam_c;
+import polyglot.ast.Node;
+import polyglot.ast.Term;
+import polyglot.ast.Term_c;
 import polyglot.ast.TypeNode;
-import polyglot.ast.TypeNode_c;
+import polyglot.types.SemanticException;
+import polyglot.types.Type;
 import polyglot.util.CodeWriter;
+import polyglot.util.InternalCompilerError;
 import polyglot.util.Position;
 import polyglot.util.SerialVersionUID;
+import polyglot.visit.CFGBuilder;
+import polyglot.visit.NodeVisitor;
 import polyglot.visit.PrettyPrinter;
+import polyglot.visit.TypeChecker;
 
-public class TypeParamNode_c extends TypeNode_c implements TypeParamNode {
+public class TypeParamNode_c extends Term_c implements TypeParamNode {
     private static final long serialVersionUID = SerialVersionUID.generate();
 
+    TypeNode typeNode;
     TypeParam typeParam;
 
     public TypeParamNode_c(Position pos, TypeNode tn) {
         super(pos);
-        // Should this type TypeParam_c be constructed here or by the TypeFactory in the parser?
-        this.typeParam = new TypeParam_c(tn);
-        super.type(tn.type());
+        typeNode = tn;
     }
 
     @Override
@@ -30,5 +39,61 @@ public class TypeParamNode_c extends TypeNode_c implements TypeParamNode {
     @Override
     public void prettyPrint(CodeWriter w, PrettyPrinter tr) {
         w.write(typeParam.toString());
+    }
+
+    @Override
+    public TypeParamNode parameter(TypeParam param) {
+        TypeParamNode_c newNode = (TypeParamNode_c) copy();
+        newNode.typeParam = param;
+        return newNode;
+    }
+
+    @Override
+    public Term firstChild() {
+        return typeNode;
+    }
+
+    @Override
+    public TypeNode typeNode() {
+        return typeNode;
+    }
+
+    @Override
+    public TypeParamNode typeNode(TypeNode type) {
+        TypeParamNode_c newNode = (TypeParamNode_c) copy();
+        newNode.typeNode = type;
+        return newNode;
+    }
+
+    @Override
+    public <T> List<T> acceptCFG(CFGBuilder<?> v, List<T> succs) {
+        return succs;
+    }
+
+    @Override
+    public Node typeCheck(TypeChecker tc) throws SemanticException {
+        TypeParamNode_c n = (TypeParamNode_c) super.typeCheck(tc);
+        Type t = n.typeNode.type();
+        JifTypeSystem jts = (JifTypeSystem) tc.typeSystem();
+        TypeParam tp = jts.typeParam(t);
+        return n.parameter(tp);
+    }
+
+    @Override
+    public Node visitChildren(NodeVisitor v) {
+        TypeNode tn = (TypeNode) visitChild(typeNode, v);
+        return reconstruct(tn);
+    }
+
+    protected TypeParamNode_c reconstruct(TypeNode tn) {
+        if (typeNode == tn) {
+            return this;
+        }
+        if (typeParam != null) {
+            throw new InternalCompilerError("Type parameter already set.");
+        }
+        TypeParamNode_c tpn = (TypeParamNode_c) copy();
+        tpn.typeNode = tn;
+        return tpn;
     }
 }
--- src/jif/types/TypeParam_c.java
+++ src/jif/types/TypeParam_c.java
@@ -1,15 +1,15 @@
 package jif.types;
 
-import polyglot.ast.TypeNode;
+import polyglot.types.Type;
 import polyglot.util.SerialVersionUID;
 
 public class TypeParam_c extends Param_c implements TypeParam {
     private static final long serialVersionUID = SerialVersionUID.generate();
 
-    public TypeNode tn;
+    public Type type;
 
-    public TypeParam_c(TypeNode tn) {
-        this.tn = tn;
+    public TypeParam_c(Type t) {
+        this.type = t;
     }
 
     @Override
