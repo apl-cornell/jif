56825b96b888ff94d04ea5427389735d74ca364c
Matthew Loring <matthew.loring21@gmail.com>
2013-11-12 10:52:08 -0500

    rewriter skeleton code for type parameters

--- src/jif/types/TypeParam.java
+++ src/jif/types/TypeParam.java
@@ -1,7 +1,12 @@
 package jif.types;
 
+import jif.translate.JifToJavaRewriter;
+import polyglot.ast.Expr;
+import polyglot.types.SemanticException;
 import polyglot.types.Type;
 
 public interface TypeParam extends Param {
     Type type();
+
+    Expr toJava(JifToJavaRewriter rw) throws SemanticException;
 }
