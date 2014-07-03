8f12123ee7e1fbdd8bf206c2e39414ea05b93d38
eliza <eliza@eliza-desktop.(none)>
2014-07-03 18:42:24 -0400

    Modifying RifReaderPolicy.java. relabelsTo needs correction.

--- rt-src/jif/lang/RifReaderPolicy.java
+++ rt-src/jif/lang/RifReaderPolicy.java
@@ -1,6 +1,7 @@
 package jif.lang;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +9,6 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import jif.lang.PrincipalUtil.DelegationPair;
-import jif.types.RifFSMstate;
-import jif.types.RifFSMstate_c;
-import jif.types.principal.Principal;
 import polyglot.ast.Id;
 import polyglot.ast.Id_c;
 
@@ -32,14 +30,28 @@ public class RifReaderPolicy extends AbstractPolicy implements ConfPolicy {
         }
     }
 
+    public RifReaderPolicy(LabelUtil labelUtil,
+            Map<String, RifFSMstate> states, RifFSMstate current) {
+        super(labelUtil);
+        this.states = states;
+        this.current = current;
+
+        allPossibleActions = new LinkedList<Id>();
+        int i;
+        for (i = 0; i < 100; i++) {
+            allPossibleActions.add(new Id_c(null, "f" + Integer.toString(i)));
+        }
+    }
+
     public RifReaderPolicy addstate(String stateId, String current,
             Set<Principal> principals) {
 
+        List<Principal> plist = new LinkedList<Principal>();
+        plist.addAll(principals);
         HashMap<String, RifFSMstate> transitions =
                 new HashMap<String, RifFSMstate>();
         RifFSMstate state =
-                new RifFSMstate_c(new Id_c(null, stateId),
-                        (List<Principal>) principals, transitions);
+                new RifFSMstate(new Id_c(null, stateId), plist, transitions);
         states.put(stateId, state);
         if (current == "true") {
             this.current = state;
@@ -65,24 +77,125 @@ public class RifReaderPolicy extends AbstractPolicy implements ConfPolicy {
         return this;
     }
 
+    public RifFSMstate currentState() {
+        return this.current;
+    }
+
     @Override
     public boolean relabelsTo(Policy p, Set<DelegationPair> s) {
+        if (this == p || this.equals(p)) return true;
+
+        if (p instanceof JoinConfPolicy) {
+            JoinPolicy jp = (JoinPolicy) p;
+            // this <= p1 join ... join p2 if there exists a pi such that
+            // this <= pi
+            for (Policy pi : jp.joinComponents()) {
+                if (labelUtil.relabelsTo(this, pi, s)) return true;
+            }
+            return false;
+        } else if (p instanceof MeetConfPolicy) {
+            return false; //do we need to fill it???
+        } else if (!(p instanceof ReaderPolicy)) return false;
+
         return false;
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return 5555678;
     }
 
     @Override
     public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof RifReaderPolicy)) {
+            return false;
+        }
+
+        RifReaderPolicy policy = (RifReaderPolicy) o;
+
+        return equalsFSM(policy, new LinkedList<String>());
+    }
+
+    public boolean equalsFSM(RifReaderPolicy pol, List<String> visited) {
+        String pair =
+                this.current.name().id() + "&" + pol.currentState().name().id();
+        List<String> newvisited = new LinkedList<String>();
+
+        if (visited.contains(pair)) {
+            return true;
+        }
+        for (String s : visited) {
+            newvisited.add(s);
+        }
+        newvisited.add(pair);
+        if (this.currentState().equals(pol.currentState())) {
+            for (Id action : allPossibleActions) {
+                if (!this.takeTransition(action).equalsFSM(
+                        pol.takeTransition(action), newvisited)) {
+                    return false;
+                }
+            }
+            return true;
+        }
         return false;
+    }
+
+    public RifReaderPolicy takeTransition(Id action) {
+        RifReaderPolicy newfsm;
+        RifFSMstate nextState = this.current.getNextState(action);
+        newfsm = new RifReaderPolicy(this.labelUtil, this.states, nextState);
+        return newfsm;
     }
 
     @Override
     public String toString() {
-        return null;
+
+        StringBuffer sb = new StringBuffer();
+
+        Iterator<Entry<String, RifFSMstate>> it =
+                this.states.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<String, RifFSMstate> stateentry = it.next();
+            sb.append(stateentry.getKey());
+            if (current == stateentry.getValue()) sb.append("*");
+            sb.append(":{");
+            List<Principal> principals = stateentry.getValue().principals();
+            if (principals != null) {
+                Iterator<Principal> ip = principals.iterator();
+                while (ip.hasNext()) {
+                    Principal p = ip.next();
+                    if (!PrincipalUtil.isTopPrincipal(p))
+                        sb.append(PrincipalUtil.toString(p));
+                    if (ip.hasNext()) {
+                        sb.append(",");
+                    }
+                }
+            }
+            sb.append("}");
+            HashMap<String, RifFSMstate> transitions =
+                    stateentry.getValue().getTransitions();
+            if (transitions != null) {
+                Iterator<Entry<String, RifFSMstate>> itt =
+                        transitions.entrySet().iterator();
+                while (itt.hasNext()) {
+                    Entry<String, RifFSMstate> pairs = itt.next();
+                    sb.append(",");
+                    sb.append(pairs.getKey());
+                    sb.append(":");
+                    sb.append(stateentry.getKey());
+                    sb.append("->");
+                    sb.append(pairs.getValue().name().toString());
+                }
+            }
+
+            if (it.hasNext()) {
+                sb.append(",");
+            }
+        }
+        return sb.toString();
+
     }
 
     @Override
